44cfee846d0fbe352e7a327fca91009427b753d8
masterAventador <419460185@qq.com>
2024-05-28 19:04:57 +0800

    add build script

--- RawData/build.sh
+++ RawData/build.sh
@@ -1,15 +1,17 @@
 cd $(dirname $0)
 
-SWIFT_GEN_DIR='../SwiftGenerated'
-CPP_GEN_DIR='../CppGenerated'
+SWIFT_GEN_DIR='../swift_gen'
+CPP_GEN_DIR='../cpp_gen'
 
-for DIR in $SWIFT_GEN_DIR $CPP_GEN_DIR
-do
-	if [ ! -d $DIR ];then
-		mkdir -p $DIR
+for SUB_DIR in $(find . -type d | sed 's|^\./||'); do 
+	GEN_DIR=$SWIFT_GEN_DIR/$SUB_DIR
+	echo $GEN_DIR
+
+	if [ ! -d $GEN_DIR ];then
+		mkdir -p $GEN_DIR
 	fi
+
+ 	protoc --swift_out=$SWIFT_GEN_DIR --swift_opt=Visibility=Public $SUB_DIR/*.proto
+	protoc --cpp_out=$CPP_GEN_DIR $SUB_DIR/*.proto
+
 done
-
-
-protoc --swift_out=$SWIFT_GEN_DIR --swift_opt=Visibility=Public *.proto
-protoc --cpp_out=$CPP_GEN_DIR *.proto
